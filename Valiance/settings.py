c458fc9f03d8714f00870a15795b01e84275f5ee
Nileshsaini <nilesh.saini6@gmail.com>
2017-03-29 13:45:00 +0530

    changed settings.py 2

--- Valiance/settings.py
+++ Valiance/settings.py
@@ -106,5 +106,10 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'templates/static'),
 # )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates/static'),
+)
